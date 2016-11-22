40caa4431cec18df8bb1d400b1f48c484654e04c
Winbobob <zhu6@ncsu.edu>
2016-11-22 16:50:55 -0500

    Add 'camelize' method in string.rb, which is the inverse method of 'underscore'.

--- lib/string.rb
+++ lib/string.rb
@@ -4,12 +4,23 @@ class String
   # so I monkey-patch underscore into String manually
   #
   def underscore
-    word = self.dup
-    word.gsub!(/::/, '/')
-    word.gsub!(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
-    word.gsub!(/([a-z\d])([A-Z])/,'\1_\2')
-    word.tr!("-", "_")
-    word.downcase!
-    word
+    string = self.dup
+    string.gsub!(/::/, '/')
+    string.gsub!(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
+    string.gsub!(/([a-z\d])([A-Z])/,'\1_\2')
+    string.tr!("-", "_")
+    string.downcase!
+    string
+  end
+
+  #
+  # camelize as the inverse of underscore
+  #
+  def camelize
+    string = self.dup
+    string = string.split('_')
+    string = string.map(&:capitalize)
+    string = string.inject('') {|word, string| word + string}
+    string
   end
 end
