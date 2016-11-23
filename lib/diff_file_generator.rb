ead8944354a4901b99290361498203cb783b23a6
Winbobob <zhu6@ncsu.edu>
2016-11-22 23:49:01 -0500

    Change lib/diff_file_generator.rb

--- lib/diff_file_generator.rb
+++ lib/diff_file_generator.rb
@@ -7,53 +7,41 @@ fault_locations = YAML.load_file("#{parent_dir_path}/mutator/results/fault-local
 fault_locations.each do |name, content|
 	airbrake_group_id = content['AirbrakeGroupId']
 	source_file_path = content['File']
+	# TODO: separate path variable into common YAML file
 	output_file_path = "#{parent_dir_path}/mutator/results/mutator-outputs/#{airbrake_group_id}"
-	directory_path = "#{parent_dir_path}/mutator/results/mutator-splited-new-methods/#{airbrake_group_id}"
+	directory_path = "#{parent_dir_path}/mutator/results/mutator-splitted-new-methods/#{airbrake_group_id}"
 
 	iterator = 1
-	temp_content = ''
-	add_line_into_temp_content = false
+	mutator_method = ''
+	add_line_into_mutator_method = false
 	puts "Separate mutators for #{airbrake_group_id}"
 
 	File.readlines(output_file_path).each do |line|
 		if line.start_with? ' def'
-			temp_content += (' ' + line)
-			add_line_into_temp_content = true
+			mutator_method += (' ' + line)
+			add_line_into_mutator_method = true
 		end
 
 		if line.start_with? '--'
 			Dir.mkdir(directory_path) unless File.exists?(directory_path)
 			full_file_path = "#{parent_dir_path}/expertiza/#{source_file_path}"
 
-			first_line = ''
-			file_name = ''
-			File.readlines(full_file_path).each do |line|
-				# find the class declaration line of origin file to
-				# become the first line of new file
-				if line.start_with? 'class'
-					first_line = line 
-					file_name = line.match(/\s[a-zA-Z]+\s/).to_s.strip.underscore
-					break
-				end
-			end
 			puts "mutator_#{iterator}"
 			mutator_diff_path = "#{directory_path}/mutator_#{iterator}"
 			file = File.open(mutator_diff_path, 'w+')
-			file.puts(first_line)
-			file.puts(temp_content)
-			file.puts('end')
+			file.puts(mutator_method)
 			file.close
 
 			iterator += 1
-			temp_content = ''
-			add_line_into_temp_content = false
+			mutator_method = ''
+			add_line_into_mutator_method = false
 		end
 
-		if add_line_into_temp_content and !line.start_with? ' def'
+		if add_line_into_mutator_method and !line.start_with? ' def'
 			if line.start_with? ' '
-				temp_content += (' ' + line)
+				mutator_method += (' ' + line)
 			elsif line.start_with? '+'
-				temp_content += (' ' + line.sub!('+', ' '))
+				mutator_method += (' ' + line.sub!('+', ' '))
 			end
 		end
 	end
