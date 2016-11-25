1c7307e417b77b4533ae5be45faa7a3d2ea0e8fb
Winbobob <zhu6@ncsu.edu>
2016-11-25 14:29:05 -0500

    Update run.sh

--- lib/run.sh
+++ lib/run.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
+echo '=====Generate mutator inputs=========='
 ruby mutator_inputs_generator.rb
 echo 'Sleep 5 seconds'
 sleep 5
 
+echo '=====Generate mutants================='
 ruby mutator.rb
 echo 'Sleep 5 seconds'
 sleep 5
 
+echo '=====Separate mutant outputs=========='
 ruby mutator_outputs_separator.rb
 echo 'Sleep 5 seconds'
 sleep 5
 
+echo '=====Validate mutants================='
 ruby mutator_validator.rb
